64cd100525dfc4399ca08f9534b8ee8332b40aac
Chris Nixon <chrisnixon@Chriss-Air.station>
2024-02-21 11:31:56 +0000

    Add command-line parsing so we can accept the URL to start crawling from as a command-line argument.

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -1,9 +1,48 @@
 package org.example;
 
+import java.util.Optional;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class App {
 
+    private static Logger log = LoggerFactory.getLogger(App.class);
     public static void main(String[] args) {
-        Crawler crawler = new SingleThreadedCrawler();
-        crawler.crawl("https://bendunnegyms.com/");
+        final Optional<String> startingURL = parseStartingURL(args);
+        if (startingURL.isPresent()) {
+            Crawler crawler = new SingleThreadedCrawler();
+            crawler.crawl(startingURL.get().trim());
+        }
+    }
+
+    private static Optional<String> parseStartingURL(String[] args) {
+        try {
+            Options options = new Options();
+            options.addOption(Option.builder("u")
+                                    .longOpt("url")
+                                    .hasArg(true)
+                                    .required(true)
+                                    .desc("Starting url to begin crawling from")
+                                    .build());
+            CommandLineParser parser = new DefaultParser();
+            CommandLine cmd = null;
+            
+            cmd = parser.parse(options, args);
+            String startingURL = cmd.getOptionValue("u");
+            return Optional.of(startingURL);
+        } catch (final ParseException e) {
+            log.error("Error parsing arguments: ", e);
+            return Optional.empty();
+        } catch (final Exception e) {
+            log.error("Unhandled error while parsing arguments: ", e);
+            return Optional.empty();
+        }
     }
 }
